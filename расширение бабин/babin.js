276f426e0e92837563e9ff03163a4a3313c73503
Babushkin05 <91776460+Babushkin05@users.noreply.github.com>
2022-07-13 08:24:38 +0700

    Add files via upload

---
+++ расширение бабин/babin.js
@@ -0,0 +1,8 @@
+var btn = document.createElement("BUTTON")
+var txt = document.createTextNode("BABNI")
+btn.appendChild(txt);
+alert("babni");
+document.head.appendChild(btn);
+var smt = document.getElementsByClassName("menu_title")
+alert(len(smt))
+smt.insertAdjastmentsHTML("<h1>babni</h1>")
---
+++ расширение бабин/babni.js
@@ -0,0 +1,8 @@
+function sound(){
+    var a = new Audio();
+    a.src = 'assets/sounds/diskoteka-avarija-pejj-pivo.mp3';
+    a.autoplay = true;
+    a.loop=false;
+}
+
+sound();
